0fce8c6d9d5114cc54848895b138e7e093f18e8d
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-01-06 23:07:33 +0500

    Update Factorial!.c

--- Factorial!.c
+++ Factorial!.c
@@ -1,3 +1,4 @@
+// Question:
 /*You have been given a positive integer N. You need to find and print the Factorial of this number. 
 The Factorial of a positive integer N refers to the product of all number in the range from 1 to N. 
 You can read more about the factorial of a number here.*/
